ba8b114665e8cf7669b89d00d6f2ca1871741335
cuilei0710 <cuilei0710@icloud.com>
2021-04-11 16:22:27 +0800

    leilei

---
+++ Java_beginner/Number_Math/compareTo.java
@@ -0,0 +1,20 @@
+package Java_beginner.Number_Math;
+
+public class compareTo {
+    public static void main(String[] args) {
+        Integer x = 5;
+        System.out.println(x.compareTo(3));
+        System.out.println(x.compareTo(5));
+        System.out.println(x.compareTo(7));
+    }
+}
+
+/*学习方法
+x
+Integer.compareTo(anotherInteger)
+anotherInteger--可以是以Byte、Double、Integer、Float、Long或Short类型的参数
+返回值
+如果指定的数与参数相等返回0
+如果指定的数小于参数返回-1
+如果指定的数大于参数返回1
+ */
